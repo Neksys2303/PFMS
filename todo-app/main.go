4b21e9eff0887c469ebf0dcac72eb3251128b33d
internet <internet@MacBook-Pro-internet.local>
2024-10-02 09:30:28 +0500

    Created a struct and a few functions

--- todo-app/main.go
+++ todo-app/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"time"
+)
+
+type Task struct {
+	id          int
+	description string
+	status      string
+	createdAt   time.Time
+	updatedAt   time.Time
+}
+
+func Add() {
+
+}
+
+func Update() {
+
+}
+
+func Delete() {
+
+}
 
 func main() {
-	fmt.Println("Hello world")
+
 }
